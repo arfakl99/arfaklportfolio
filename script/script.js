45a10b6c13785fa48ff34f9e216767e9bc99c2cf
Muhammad Arif <66298095+arfakl99@users.noreply.github.com>
2022-07-03 19:05:20 +0500

    portfolio website
    personal portfolio website

---
+++ script/script.js
@@ -0,0 +1,24 @@
+// // // add class navbarDark on navbar scroll
+// // const header = document.querySelector('.navbar');
+window.onscroll = function () {
+    var top = window.scrollY;
+    if (top >= 100) {
+        header.classList.add('navbarDark');
+    }
+    else {
+        header.classList.remove('navbarDark');
+    }
+}
+
+
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+
